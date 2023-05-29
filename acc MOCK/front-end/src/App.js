a68f8e4b1bd3a812eefeda5cd769879ca5460ffd
hiep464 <luuhiep2309@gmail.com>
2023-05-29 22:54:18 +0700

    fix: remove file git in folder

---
+++ acc MOCK/front-end/src/App.js
@@ -0,0 +1,43 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes/Routes';
+import DefaultLayout from './layout/defaultLayout/DefaultLayout';
+import { Fragment } from 'react';
+import GlobalStyles from './components/GlobalStyles';
+import { AuthContextProvider } from './context/AuthContext';
+
+function App() {
+    return (
+        <AuthContextProvider>
+            <BrowserRouter>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.element;
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (!route.layout) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <GlobalStyles>
+                                        <Layout>
+                                            <Page />
+                                        </Layout>
+                                    </GlobalStyles>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </BrowserRouter>
+        </AuthContextProvider>
+    );
+}
+
+export default App;
